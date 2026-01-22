6dea6a89aa1c737a792f65795a7b8a835fc77f54
Cauan Diniz <cauanrodiniz@gmail.com>
2026-01-22 06:41:58 -0300

    test: ensure AxiosHttpClient returns correct values
    Signed-off-by: Cauan Diniz <cauanrodiniz@gmail.com>

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,4 +1,4 @@
-import { HttpPostParams } from '@/data';
+import { HttpPostParams } from '@/data/protocols';
 import { AxiosHttpClient } from '@/infra/http';
 import { faker } from '@faker-js/faker';
 import axios from 'axios';
@@ -6,6 +6,13 @@ import axios from 'axios';
 jest.mock('axios');
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+const mockedAxiosResult = {
+  data: faker.helpers.objectValue({
+    any_value: 'any_value',
+  }),
+  status: faker.number,
+};
+mockedAxios.post.mockResolvedValue(mockedAxiosResult);
 
 type SutTypes = {
   sut: AxiosHttpClient;
@@ -37,5 +44,19 @@ describe('AxiosHttpClient', () => {
 
     // Assert
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
+  });
+
+  it('should return the correct statusCode and body', async () => {
+    // Arrange
+    const { sut } = makeSut();
+
+    // Act
+    const result = await sut.post(mockPostRequest());
+
+    // Assert
+    expect(result).toEqual({
+      statusCode: mockedAxiosResult.status,
+      body: mockedAxiosResult.data,
+    });
   });
 });
