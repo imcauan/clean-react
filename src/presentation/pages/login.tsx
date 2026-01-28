4f707f9d7b1eef00195aac5b34fc5e8872ebbe9d
Cauan Diniz <cauanrodiniz@gmail.com>
2026-01-28 07:45:47 -0300

    feat: add LoginPage component and Spinner component; update globals.css and package.json
    Signed-off-by: Cauan Diniz <cauanrodiniz@gmail.com>

---
+++ src/presentation/pages/login.tsx
@@ -0,0 +1,53 @@
+export function LoginPage() {
+  return (
+    <div className="flex flex-col w-screen h-screen justify-between bg-background">
+      <header className="bg-primary">
+        <h1 className="p-4 text-white">4Dev - Enquetes para programadores</h1>
+      </header>
+      <form className="flex items-center justify-center">
+        <div className="flex flex-col gap-4 bg-white p-10 shadow-md">
+          <h2 className="text-2xl font-bold text-center uppercase text-primary">
+            Login
+          </h2>
+          <div className="flex flex-col">
+            <label htmlFor="email" className="mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Digite seu email"
+              className="border w-md border-primary leading-10 rounded-md pl-3"
+            />
+            <span className="text-red-600 mt-1">Email inválido</span>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password" className="mb-1">
+              Senha
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Digite seu senha"
+              className="border w-md border-primary leading-10 rounded-md pl-3"
+            />
+            <span className="text-red-600 mt-1">Senha inválida</span>
+          </div>
+          <button
+            type="submit"
+            className="bg-primary w-md text-white rounded-md p-2 cursor-pointer hover:opacity-90 flex justify-center items-center"
+          >
+            Entrar
+          </button>
+          <a className="text-center text-primary hover:underline cursor-pointer">
+            Criar conta
+          </a>
+          <div className="w-full flex justify-center my-4">
+            <p className="text-primary-light">Erro</p>
+          </div>
+        </div>
+      </form>
+      <footer className="bg-primary h-8">hello!</footer>
+    </div>
+  );
+}
